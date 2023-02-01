51b546b61fa49d210efea4fa18029f6879b32b92
andryushchenkoka <kirill2goo@gmail.com>
2023-02-01 04:54:45 +0300

    allure: custom template

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,9 +1,19 @@
 package tests;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+import static helpers.CustomAllureListener.*;
+
 public class BaseTest {
 
     protected String URL = "https://reqres.in/",
             USERS_ENDPOINT = "api/users/",
             REGISTER_ENDPOINT = "/api/register/",
             LOGIN_ENDPOINT = "/api/login";
+
+    @BeforeAll
+    public static void beforeAll() {
+        RestAssured.filters(withCustomTemplate());
+    }
 }
