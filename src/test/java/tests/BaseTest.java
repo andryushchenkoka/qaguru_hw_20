9a3a22f129a2ed8257f9260ff6cb3b2ad22caab5
andryushchenkoka <kirill2goo@gmail.com>
2023-02-01 03:16:10 +0300

    Modified with Groovy

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,7 +2,7 @@ package tests;
 
 public class BaseTest {
 
-    String URL = "https://reqres.in/",
+    protected String URL = "https://reqres.in/",
             USERS_ENDPOINT = "api/users/",
             REGISTER_ENDPOINT = "/api/register/",
             LOGIN_ENDPOINT = "/api/login";
---
+++ src/test/java/tests/groovy/GroovyTests.java
@@ -0,0 +1,47 @@
+package tests.groovy;
+
+import org.junit.jupiter.api.*;
+import tests.BaseTest;
+
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+import static specs.Specs.*;
+import static specs.Specs.responseSpec200;
+import static tags.Tags.GET_REQUEST;
+import static tags.Tags.POSITIVE;
+
+public class GroovyTests extends BaseTest {
+
+    @Test
+    @DisplayName("Количество элементов на странице взаимоизменяется (c groovy)")
+    @Tags({@Tag(POSITIVE), @Tag(GET_REQUEST)})
+    public void getUsersListGroovyTest() {
+        step("Получить список пользователей на 2 странице", () -> {
+            useSpecs(requestSpec(URL), responseSpec200());
+            given()
+                    .when()
+                    .param("page", 2)
+                    .param("per_page", 8)
+                    .get(USERS_ENDPOINT)
+                    .then().log().all()
+                    .body("data.findAll{it.id}.size()", is(4))
+                    .extract().response();
+        });
+    }
+
+    @Test
+    @DisplayName("Email содержит имя пользователя (c groovy)")
+    @Tags({@Tag(POSITIVE), @Tag(GET_REQUEST)})
+    public void getUserGroovyTest() {
+        step("Email содержит имя пользователя", () -> {
+            useSpecs(requestSpec(URL), responseSpec200());
+            given()
+                    .param("id", 3)
+                    .get(USERS_ENDPOINT)
+                    .then().log().all()
+                    .body("data.email", is("emma.wong@reqres.in"))
+                    .extract().response();
+        });
+    }
+}
